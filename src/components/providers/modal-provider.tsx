132c29d361320201dbb15ac6b0e471701b95bde9
LooKes Wong <lookes.wong@kingdomdigital.com.my>
2024-05-12 02:07:14 +0800

    minor update

--- src/components/providers/modal-provider.tsx
+++ src/components/providers/modal-provider.tsx
@@ -3,20 +3,31 @@
 import { useEffect, useState } from "react";
 
 import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { InviteModal } from "@/components/modals/invite-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
+import { MembersModal } from "@/components/modals/members-modal";
+import { CreateChannelModal } from "@/components/modals/create-channel-modal";
+import { LeaveServerModal } from "@/components/modals/leave-server-modal";
+import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 
 export const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
 
-  const [isMounted, setIsMounted] = useState(false);
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
 
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
+    if (!isMounted) return null;
 
-  if (!isMounted) return null;
-
-  return (
-    <>
-      <CreateServerModal />
-    </>
-  )
-}
+    return (
+        <>
+            <CreateServerModal />
+            <InviteModal />
+            <EditServerModal />
+            <MembersModal />
+            <CreateChannelModal />
+            <LeaveServerModal />
+            <DeleteServerModal />
+        </>
+    );
+};
